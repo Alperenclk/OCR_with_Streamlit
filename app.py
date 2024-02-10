9a02129e6470897e47a9715ce8a07cd63bdca2c9
alperenclk <alperenclk1876@gmail.com>
2024-02-10 22:03:58 +0300

    start project

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from doctr.models import ocr_predictor
+from doctr.io import DocumentFile
+
+
+st.title("OCR App")
+
+uploaded_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    image = uploaded_file.read()
+
+    model = ocr_predictor("db_resnet50", "crnn_vgg16_bn", pretrained=True)
+    single_img_doc = DocumentFile.from_images(image)
+
+    result = model(single_img_doc)
+    json_output = result.export()
+
+    # Show the results
+    counter = 0
+    for block in json_output["pages"][0]["blocks"]:
+        for line in block["lines"]:
+            counter += 1
+            for word in line["words"]:
+                st.write(f"### Word {counter}:", word["value"])
