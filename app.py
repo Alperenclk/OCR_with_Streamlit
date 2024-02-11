1996f3b83c3dcd2efad108a8b661baa83632e4bb
alperenclk <alperenclk1876@gmail.com>
2024-02-11 09:54:59 +0300

    add json file

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ TODO:
     - Dockerize et
     - zamanlayici ekle
     - diger dosya tiplerini ekle
+    - json dosyalarini upload et
 """
 
+import base64
+import json
 import time
 import warnings
+from threading import Timer
+
+import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
-from threading import Timer
-import matplotlib.pyplot as plt
 from doctr.io import DocumentFile
 from doctr.models import ocr_predictor
 
@@ -24,8 +28,16 @@ st.title("Image to Text App")
 
 def putMarkdown():
     svg_code = """<svg width="100%" height="5"><line x1="0" y1="5" x2="100%" y2="5" stroke="black" stroke-width="1"/></svg>"""
-    st.write(svg_code, unsafe_allow_html=True)
+    # st.write(svg_code, unsafe_allow_html=True)
     st.markdown("<hr>", unsafe_allow_html=True)
+
+
+def get_download_button(data, button_text, filename):
+    # JSON verisini dosyaya yaz
+    json_str = json.dumps(data, indent=4)
+    b64 = base64.b64encode(json_str.encode()).decode()
+    href = f'<a href="data:application/json;charset=utf-8;base64,{b64}" download="{filename}">{button_text}</a>'
+    return href
 
 
 def main():
@@ -46,6 +58,14 @@ def main():
         result = model(single_img_doc)
         json_output = result.export()
 
+        st.write("### Downoad Json output")
+        st.write("**⬇**" * 10)
+
+        # Button of Download JSON
+        download_button_str = get_download_button(json_output, "DOWNLOAD", "data.json")
+        st.markdown(download_button_str, unsafe_allow_html=True)
+        putMarkdown()
+
         # Show the results
         whole_words = []
         for block in json_output["pages"][0]["blocks"]:
@@ -57,14 +77,11 @@ def main():
         st.write(f"## Whole Words:")
         st.write(i + " " for i in whole_words)
         putMarkdown()
-
         for index, item in enumerate(whole_words):
             st.write(f"**Word {index}**:", item)
 
-        # Put the markdown
-        putMarkdown()
-
         # Show the result image
+        putMarkdown()
         synthetic_pages = result.synthesize()
         # new_width = 680
         # new_height = 960
